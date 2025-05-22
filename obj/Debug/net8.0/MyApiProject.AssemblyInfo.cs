d28e5e5fb68dda80d48382505279e6b91893fcad
hoangbatuocdeptrai <102572153+hoangbatuocdeptrai@users.noreply.github.com>
2025-05-22 19:00:49 +0700

    lan2

--- obj/Debug/net8.0/MyApiProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApiProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c81e8bc731415cb08fd812315f5b237f44565900")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
